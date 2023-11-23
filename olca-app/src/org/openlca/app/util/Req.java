37f84a9fc5e59eb7b45cfbdd06b5a1145ec30a0b
eericxu <2681350846@qq.com>
2023-11-23 14:13:28 +0800

    release 1.0.4
    	Support login
    	Support Sync data use gitlab

--- olca-app/src/org/openlca/app/util/Req.java
+++ olca-app/src/org/openlca/app/util/Req.java
@@ -11,22 +11,20 @@ public class Req {
 	private static HttpClient client;
 	private static Gson gson = new Gson();
 	// beta
-	 private static final String base = "https://pre-api.gtech.world";
+//	 private static final String base = "https://pre-api.gtech.world";
 	// prod
-//	private static final String base = "https://api-v2.gtech.world";
+	private static final String base = "https://api-v2.gtech.world";
 
 	public static <T> T httpGET(String path, Class<T> classz) throws Exception {
 		var req = HttpRequest.newBuilder(URI.create(base + path)).build();
 		var res = instanceClinet().send(req, HttpResponse.BodyHandlers.ofString());
 		return gson.fromJson(res.body(), classz);
 	}
-	
-	public static <T> T httpPost(String path,String body, Class<T> classz) throws Exception {
+
+	public static <T> T httpPost(String path, String body, Class<T> classz) throws Exception {
 		var bodyPub = HttpRequest.BodyPublishers.ofString(body);
 		var req = HttpRequest.newBuilder(URI.create(base + path))
-				.header("Content-Type", "application/json;charset=utf-8")
-				.POST(bodyPub)
-				.build();
+				.header("Content-Type", "application/json;charset=utf-8").POST(bodyPub).build();
 		var res = instanceClinet().send(req, HttpResponse.BodyHandlers.ofString());
 		return gson.fromJson(res.body(), classz);
 	}
@@ -35,11 +33,12 @@ public class Req {
 		public int status;
 		public String message;
 		public T data;
-		public RES(){
-			
+
+		public RES() {
+
 		}
 	}
-	
+
 	private static HttpClient instanceClinet() {
 		if (client == null) {
 			client = HttpClient.newHttpClient();
